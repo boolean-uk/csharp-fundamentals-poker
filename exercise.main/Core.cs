e8f7672c9ec2af1a715437aee98b9afec80e4072
guro18 <rollgustav95@gmail.com>
2024-01-11 17:14:42 +0100

    poker exercise Gustav Roll

--- exercise.main/Core.cs
+++ exercise.main/Core.cs
@@ -15,18 +15,58 @@ namespace exercise.main
         //TODO: complete the following method, keeping the signature the same
         public bool winningPair(IEnumerable<Tuple<string, string>> hand, out Tuple<string, string> result)
         {
-            result = new Tuple<string,string>(string.Empty, string.Empty);
-           
+            // Inetger to check highest value
+            int highestHand = 0;
+            
+            //resultTuple
+            result = new Tuple<string, string>(string.Empty, string.Empty);
 
-          
-
-          
+            //Loop through all tuples and replace currently highest one if there is higher pair
+            foreach (var pair in hand) 
+            {
+                if (pair.Item1 == pair.Item2 && GetValueOfCard(pair.Item1) > highestHand)
+                {
+                    highestHand = (GetValueOfCard(pair.Item1));
+                    result = pair;
+                   
+                }
+            }
 
             return result.Item1!=string.Empty ? true : false;
+
         }
         public int GetValueOfCard(string card)
         {
-            return  0;           
+            int result = 0;
+
+            // try to convert to int, and check if the value is between 2 and 10
+            if (int.TryParse(card, out int cardValue) && cardValue >= 2 && cardValue <= 10 ) 
+            {
+                result = cardValue;
+            }
+            //otherwise, convert following chars to corresponding ints
+            else
+            {
+                switch (card)
+                {
+                    case "J":
+                        result = 11;
+                        break;
+
+                    case "Q":
+                        result = 12;
+                        break;
+
+                    case "K":
+                        result = 13;
+                        break;
+
+                    case "A":
+                        result = 14;
+                        break;
+                }
+            }; 
+            return result;
         }
     }
 }
