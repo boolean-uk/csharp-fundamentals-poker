e2bf95185e22ec671d525ee2d32431821b15d485
Tølløv Aresvik <tollov.aresvik@no.experis.com>
2024-01-11 14:35:39 +0100

    Solved core

--- exercise.main/Core.cs
+++ exercise.main/Core.cs
@@ -10,23 +10,39 @@ namespace exercise.main
 {
     public class Core
     {
-       
+       private Dictionary<string, int> _specialCardValues = new Dictionary<string, int>
+       {
+           {"J", 11}, {"Q", 12}, {"K", 13}, {"A", 14}
+       };
+
         
         //TODO: complete the following method, keeping the signature the same
         public bool winningPair(IEnumerable<Tuple<string, string>> hand, out Tuple<string, string> result)
         {
-            result = new Tuple<string,string>(string.Empty, string.Empty);
-           
-
-          
-
-          
-
+            result = new Tuple<string, string>(string.Empty, string.Empty);
+            foreach (Tuple<string,string> pair in hand)
+            {
+                if (pair.Item1 == pair.Item2)
+                {
+                    if (GetValueOfCard(pair.Item1) > GetValueOfCard(result.Item1))
+                    {
+                        result = pair;
+                    }
+                }
+            }
             return result.Item1!=string.Empty ? true : false;
         }
         public int GetValueOfCard(string card)
         {
-            return  0;           
+            if (card.Length == 0)
+            {
+                return 0;
+            }
+            if (_specialCardValues.ContainsKey(card))
+            {
+                return _specialCardValues[card];
+            }
+            return int.Parse(card);
         }
     }
 }
