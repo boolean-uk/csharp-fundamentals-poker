11c0507cf457ad7680204a851b20cc46996a488c
Ole Markus Roland <ole.markus.roland@no.experis.com>
2024-01-11 10:10:54 +0100

    Completed Core and Extension. Also created tests for Extension. Only the ungradede part of the Extension remains.

--- exercise.main/Extension.cs
+++ exercise.main/Extension.cs
@@ -6,15 +6,58 @@ using System.Threading.Tasks;
 
 namespace exercise.main
 {
+    public class Card(char value, string suit)
+    {
+        public char Value { get; set; }
+        public string Suit { get; set; }
+
+    }
+    public class Deck(List<Card> cards)
+    {
+        public List<Card> cards { get; set; }
+    }
     public class Extension
     {
         //TODO: complete the following method, keeping the signature the same
         public bool winningThree(IEnumerable<Tuple<string, string, string>> hand, out Tuple<string, string, string> result)
         {
             result = new Tuple<string, string, string>(string.Empty, string.Empty, string.Empty);
+            int higestScore = 0;
+            int newScore = 0;
+            foreach (var pair in hand)
+            {
+                if (pair.Item1 == pair.Item2 && pair.Item2 == pair.Item3)
+                {
+                    newScore = GetValueOfCard(pair.Item1);
+                    if (newScore > higestScore)
+                    {
+                        higestScore = newScore;
+                        result = pair;
+                    }
+                }
 
-            return false;
+            }
+            return result.Item1 != string.Empty ? true : false;
         }
-
+        public int GetValueOfCard(string card)
+        {
+            int score = 0;
+            bool result = int.TryParse(card, out score);
+            if (!result)
+            {
+                switch (card)
+                {
+                    case "J":
+                        score = 11; break;
+                    case "Q":
+                        score = 12; break;
+                    case "K":
+                        score = 13; break;
+                    case "A":
+                        score = 14; break;
+                }
+            }
+            return score;
+        }
     }
 }
--- exercise.main/Program.cs
+++ exercise.main/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using exercise.main;
+
+Extension extension = new Extension();
+
+List<Tuple<string, string, string>> hand = new List<Tuple<string, string, string>>
+        {
+            new Tuple<string, string, string>("K", "K", "K"),
+            new Tuple<string, string, string>("A","A", "A")
+        };
+Tuple<string, string, string> winner;
+
+bool result = extension.winningThree(hand, out winner);
 Console.WriteLine("Hello, World!");
