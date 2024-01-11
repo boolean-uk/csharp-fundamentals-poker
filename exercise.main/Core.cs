7f993fda508a17d843c4df0fa966eec8f71a903b
Mahmoud Ali <mahmoodali2965.ma@gmail.com>
2024-01-11 13:47:20 +0100

    Mahmoud Ali

--- exercise.main/Core.cs
+++ exercise.main/Core.cs
@@ -16,17 +16,48 @@ namespace exercise.main
         public bool winningPair(IEnumerable<Tuple<string, string>> hand, out Tuple<string, string> result)
         {
             result = new Tuple<string,string>(string.Empty, string.Empty);
-           
 
-          
 
-          
+            var groupedCards = hand.GroupBy(card => card.Item1);
 
-            return result.Item1!=string.Empty ? true : false;
+
+            var pairGroup = groupedCards.FirstOrDefault(group => group.Count() >= 2);
+
+            if (pairGroup != null)
+            {
+                // Get the highest pair value
+                var pairValue = GetValueOfCard(pairGroup.Key);
+
+                // Find the highest pair in the hand
+                var highestPair = hand.Where(card => card.Item1 == pairGroup.Key)
+                                      .OrderByDescending(card => GetValueOfCard(card.Item2))
+                                      .FirstOrDefault();
+
+                result = highestPair;
+                return true;
+            }
+
+            return false;
+
+
+           // return result.Item1!=string.Empty ? true : false;
         }
         public int GetValueOfCard(string card)
         {
-            return  0;           
+            switch (card)
+            {
+                case "J": return 11;
+                case "Q": return 12;
+                case "K": return 13;
+                case "A": return 14;
+                default:
+                    if (int.TryParse(card, out int value))
+                        return value;
+                    else
+                        return 0; // Invalid card value
+
+
+            }
         }
     }
 }
