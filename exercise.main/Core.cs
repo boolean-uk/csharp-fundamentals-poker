bb874cb444978458a8d51ad48072f1969c86389f
Najem Hamo <najem.hamo@gmail.com>
2024-01-11 11:35:57 +0100

    done with the core exercise

--- exercise.main/Core.cs
+++ exercise.main/Core.cs
@@ -13,20 +13,91 @@ namespace exercise.main
        
         
         //TODO: complete the following method, keeping the signature the same
-        public bool winningPair(IEnumerable<Tuple<string, string>> hand, out Tuple<string, string> result)
+
+        public bool winningPair(IEnumerable<Tuple<string, string>> hands, out Tuple<string, string> result)
+    {
+        Dictionary<int, List<Tuple<string, string>>> valueToCards = new Dictionary<int, List<Tuple<string, string>>>();
+
+        foreach (var cardPair in hands)
         {
-            result = new Tuple<string,string>(string.Empty, string.Empty);
+            int value1 = GetValueOfCard(cardPair.Item1);
+            int value2 = GetValueOfCard(cardPair.Item2);
+
            
+            if (!valueToCards.ContainsKey(value1))
+                valueToCards[value1] = new List<Tuple<string, string>>();
+            valueToCards[value1].Add(cardPair);
 
-          
+            if (value1 != value2)
+            {
+                if (!valueToCards.ContainsKey(value2))
+                    valueToCards[value2] = new List<Tuple<string, string>>();
+                valueToCards[value2].Add(cardPair);
+            }
 
-          
-
-            return result.Item1!=string.Empty ? true : false;
+            if (value1 == value2)
+            {
+                if (!valueToCards.ContainsKey(value1))
+                    valueToCards[value1] = new List<Tuple<string, string>>();
+                valueToCards[value1].Add(cardPair);
+            }           
         }
-        public int GetValueOfCard(string card)
+
+        int maxValue = -1;
+        Tuple<string, string> winningPair = null;
+
+        foreach (var pair in valueToCards)
         {
-            return  0;           
+            if (pair.Value.Count >= 2 && pair.Key > maxValue)
+            {
+                maxValue = pair.Key;
+                winningPair = pair.Value[0];
+            }
+        }
+
+        result = winningPair ?? new Tuple<string, string>(string.Empty, string.Empty);
+
+        return winningPair != null;
+    }
+
+    public int GetValueOfCard(string card)
+    {
+        switch (card)
+        {
+            case "2":
+                return 2;
+            case "3":
+                return 3;
+            case "4":
+                return 4;
+            case "5":
+                return 5;
+            case "6":
+                return 6;
+            case "7":
+                return 7;
+            case "8":
+                return 8;
+            case "9":
+                return 9;
+            case "10":
+                return 10;
+            case "J":
+                return 11;
+            case "Q":
+                return 12;
+            case "K":
+                return 13;
+            case "A":
+                return 14; 
+            default:
+                if (int.TryParse(card, out int value))
+                    return value;
+                return 0;
         }
     }
+    }
 }
+
+
+
