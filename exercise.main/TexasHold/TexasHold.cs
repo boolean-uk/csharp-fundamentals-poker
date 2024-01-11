f36bf328f6392bea5835b775a29a55584942d0fe
Kanthee Kosolyuthasarn <Kanthee>
2024-01-11 14:51:32 +0100

    Kanthee Kosolyuthasarn - C#3 - 11.01.2024, commmit2

--- exercise.main/TexasHold/TexasHold.cs
+++ exercise.main/TexasHold/TexasHold.cs
@@ -4,9 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace exercise.main
+namespace exercise.main.TexasHold
 {
-    internal class TexasHold
+    public class TexasHold
     {
+
     }
 }
