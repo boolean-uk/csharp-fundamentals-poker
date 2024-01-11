b446331df8796bb935bc0b2cedb4b70b9ee9e061
Kanthee Kosolyuthasarn <Kanthee>
2024-01-11 14:48:05 +0100

    Kanthee Kosolyuthasarn - C#3 - 11.01.2024

--- exercise.main/Extension.cs
+++ exercise.main/Extension.cs
@@ -6,15 +6,54 @@ using System.Threading.Tasks;
 
 namespace exercise.main
 {
-    public class Extension
+    public class Extension:Core
     {
+        public Dictionary<string, int> cards2;
+        public Extension() 
+        {
+            cards2 = new Dictionary<string, int>(base.cards);
+        }
+
         //TODO: complete the following method, keeping the signature the same
         public bool winningThree(IEnumerable<Tuple<string, string, string>> hand, out Tuple<string, string, string> result)
         {
+            List<(string, string, string)> temp = new List<(string, string, string)>();
             result = new Tuple<string, string, string>(string.Empty, string.Empty, string.Empty);
 
-            return false;
+
+            foreach ((string card1, string card2, string card3) in hand)  // Or var tuple in hand
+            {
+               
+
+                //Check if the cards are in treplet:
+                if (GetValueOfCard(card1) == GetValueOfCard(card2) && GetValueOfCard(card1) == GetValueOfCard(card3))
+                {
+                    temp.Add((card1, card2, card3));
+                }
+            }
+
+            // Check if several winning hands:
+            if (temp.Count > 1)
+            {
+                int checkHigest = 0;
+                foreach ((string card1, string card2, string card3) in temp)
+                {
+                    if (checkHigest < GetValueOfCard(card1))
+                    {
+                        checkHigest = GetValueOfCard(card1);
+                        result = new Tuple<string, string, string>(card1, card2, card3);
+                    }
+                }
+            }
+
+            return result.Item1 != string.Empty ? true : false;
+
+           
+            
         }
+
+        
+       
 
     }
 }
---
+++ exercise.tests/ExtensionTests.cs
@@ -0,0 +1,92 @@
+﻿using exercise.main;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise.tests
+{
+    public class ExtensionTests
+    {
+        //{("K","5","5"),("3","7","7")} => false out ("","","")
+        [Test]
+        public void ScenarioE1()
+        {
+
+            // Access
+            Extension extension = new Extension();
+
+            //Model
+
+            List<Tuple<string, string, string>> hand = new List<Tuple<string, string, string>>
+        {
+            new Tuple<string, string, string>("K", "5", "5"),
+            new Tuple< string, string, string >("3","7", "7")
+        };
+            Tuple<string, string, string> winner;
+
+            bool result = extension.winningThree(hand, out winner);
+
+
+            // Assert
+            Assert.That(result, Is.False);
+            Assert.That(winner.Item1, Is.EqualTo(String.Empty));
+            Assert.That(winner.Item2, Is.EqualTo(String.Empty));
+            Assert.That(winner.Item3, Is.EqualTo(String.Empty));
+
+        }
+
+
+        //{("5","5","5"),("7","7","7")} => true out ("7","7","7")
+        [Test]
+        public void ScenarioE2()
+        {
+
+            // Access
+            Extension extension = new Extension();
+
+            //Model
+
+            List<Tuple<string, string, string>> hand = new List<Tuple<string, string, string>>
+        {
+            new Tuple<string, string, string>("5", "5", "5"),
+            new Tuple< string, string, string >("7","7", "7")
+        };
+            Tuple<string, string, string> winner;
+
+            bool result = extension.winningThree(hand, out winner);
+
+
+            // Assert
+            Assert.That(result, Is.True);
+            Assert.That(winner.Item1, Is.EqualTo("7"));
+            Assert.That(winner.Item2, Is.EqualTo("7"));
+            Assert.That(winner.Item3, Is.EqualTo("7"));
+
+        }
+
+        //{("4", "3", "4"),("6","6", "6"),("7","7", "7"),("3","3", "3")}  => true ("7", "7", "7")
+        [Test]
+        public void ScenarioE3()
+        {
+            Extension extension = new Extension();
+
+            List<Tuple<string, string, string>> hand = new List<Tuple<string, string, string>> { 
+
+            new Tuple<string, string, string>("4", "3", "4"),
+            new Tuple< string, string, string >("6", "6", "6"),
+            new Tuple < string, string, string > ("7",  "7", "7"),
+            new Tuple < string, string, string > ("3", "3", "3")
+        };
+            Tuple<string, string, string> winner;
+            bool result = extension.winningThree(hand, out winner);
+
+            Assert.That(result, Is.True);
+
+            Assert.IsTrue(winner.Item1 == "7" && winner.Item2 == "7" && winner.Item3 == "7");
+
+        }
+
+    }
+}
