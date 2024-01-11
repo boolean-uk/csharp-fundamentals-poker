c5c58e00d61f69cc5d34f86be49eb4b8ba8ea26d
Stian Gaustad <stian.gaustad@no.experis.com>
2024-01-11 13:36:21 +0100

    Extension solved

--- exercise.main/Extension.cs
+++ exercise.main/Extension.cs
@@ -11,10 +11,20 @@ namespace exercise.main
         //TODO: complete the following method, keeping the signature the same
         public bool winningThree(IEnumerable<Tuple<string, string, string>> hand, out Tuple<string, string, string> result)
         {
-            result = new Tuple<string, string, string>(string.Empty, string.Empty, string.Empty);
+            Core core = new Core();
+            Tuple<string, string, string> emptyHand = new Tuple<string, string, string>(string.Empty, string.Empty, string.Empty);
 
-            return false;
+            // Pick out all winning hands, with a sorting of highest on top/first.
+            IEnumerable<Tuple<string, string, string>> winningHands = hand
+                .Where(a => ((a.Item1 == a.Item2) && (a.Item2 == a.Item3)))
+                .OrderByDescending(a => core.GetValueOfCard(a.Item1));
+            // Pick the first tuple, if none exists then pick the emptyHand defined earlier.
+            result = winningHands.FirstOrDefault(emptyHand);
+
+            return (result.Item1 != string.Empty ? true : false);
+
         }
+
 
     }
 }
