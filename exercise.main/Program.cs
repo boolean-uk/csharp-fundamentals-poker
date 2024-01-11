c29d59ef6ec05e03553e03a21498304240083b63
Victor <victor.adamson@se.experis.com>
2024-01-11 15:48:26 +0100

    Completed Core requirements

--- exercise.main/Program.cs
+++ exercise.main/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
+
+using exercise.main;
+
+Core core = new Core();
+
+List<Tuple<string, string>> hand = new List<Tuple<string, string>>
+        {
+            new Tuple<string, string>("K", "K"),
+            new Tuple<string, string>("A", "A"),
+
+        };
+Tuple<string, string> winner;
+
+bool result = core.winningPair(hand, out winner);
