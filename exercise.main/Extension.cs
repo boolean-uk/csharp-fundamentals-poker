3b34abbba3dd7ad42f57cca7ef9de6b3e5c8aa06
Jacob Chen Jensen <jacob.chen.jensen@dk.experis.com>
2024-01-11 11:23:51 +0100

    core and extension

--- exercise.main/Extension.cs
+++ exercise.main/Extension.cs
@@ -13,7 +13,61 @@ namespace exercise.main
         {
             result = new Tuple<string, string, string>(string.Empty, string.Empty, string.Empty);
 
-            return false;
+            int currentHighest = 0;
+            int potentialHigher = 0;
+            int hands = hand.Count();
+
+            foreach (Tuple<string, string, string> three in hand)
+            {
+                if (three.Item1 == three.Item2 && three.Item1 == three.Item3)
+                {
+                    potentialHigher = GetValueOfCard(three.Item1);
+                    if (potentialHigher > currentHighest)
+                    {
+                        currentHighest = potentialHigher;
+                        result = three;
+                    }
+                }
+            }
+
+
+            return result.Item1 != string.Empty ? true : false;
+        }
+
+
+
+        public int GetValueOfCard(string card)
+        {
+            switch (card)
+            {
+                case ("2"):
+                    return 2;
+                case ("3"):
+                    return 3;
+                case ("4"):
+                    return 4;
+                case ("5"):
+                    return 5;
+                case ("6"):
+                    return 6;
+                case ("7"):
+                    return 7;
+                case ("8"):
+                    return 8;
+                case ("9"):
+                    return 9;
+                case ("10"):
+                    return 10;
+                case ("J"):
+                    return 11;
+                case ("Q"):
+                    return 12;
+                case ("K"):
+                    return 13;
+                case ("A"):
+                    return 14;
+            }
+            return 0;
         }
 
     }
---
+++ exercise.tests/ExtensionTests.cs
@@ -0,0 +1,119 @@
+﻿using exercise.main;
+using Newtonsoft.Json.Serialization;
+
+namespace exercise.tests;
+
+public class ExtensionTests
+{
+
+    //{("K","5"),("3","7")} => false out ("","")
+    [Test]
+    public void Scenario1()
+    {
+
+
+        Extension extension = new Extension();
+
+        List<Tuple<string, string, string>> hand = new List<Tuple<string, string, string>>
+        {
+            new Tuple< string, string, string >("K", "5", "2"),
+            new Tuple< string, string, string >("3","7", "2")
+        };
+        Tuple<string, string, string> winner;
+
+        bool result = extension.winningThree(hand, out winner);
+
+        Assert.That(result, Is.False);
+        Assert.That(winner.Item1, Is.EqualTo(String.Empty));
+        Assert.That(winner.Item2, Is.EqualTo(String.Empty));
+        Assert.That(winner.Item3, Is.EqualTo(String.Empty));
+
+    }
+
+    //{("K","5"),("2","2")} => true out ("2","2")
+    public void Scenario1b()
+    {
+
+
+        Extension extension = new Extension();
+
+        List<Tuple<string, string, string>> hand = new List<Tuple<string, string, string>>
+        {
+            new Tuple< string, string, string >("K", "5","2"),
+            new Tuple< string, string, string >("2","2","2")
+        };
+        Tuple<string, string, string> winner;
+
+        bool result = extension.winningThree(hand, out winner);
+
+        Assert.That(result, Is.False);
+        Assert.That(winner.Item1, Is.EqualTo("2"));
+        Assert.That(winner.Item2, Is.EqualTo("2"));
+        Assert.That(winner.Item3, Is.EqualTo("2"));
+
+    }
+
+    //("K","K"), ("A","A")} => true out ("A","A")
+    [Test]
+    public void Scenario2()
+    {
+
+
+        Extension extension = new Extension();
+
+        List<Tuple<string, string,string>> hand = new List<Tuple<string, string, string>>
+        {
+            new Tuple < string, string, string > ("K", "K", "K"),
+            new Tuple < string, string, string > ("A", "A", "A")
+        };
+        Tuple<string, string, string> winner;
+        bool result = extension.winningThree(hand, out winner);
+
+        Assert.That(result, Is.True);
+
+        Assert.IsTrue(winner.Item1 == "A" && winner.Item2 == "A" && winner.Item3 == "A");
+
+    }
+    //{("4", "3"),("6","6"),("7","7"),("3","3")}  => true ("7", "7")
+    [Test]
+    public void Scenario3()
+    {
+        Extension extension = new Extension();
+
+        List<Tuple<string, string, string>> hand = new List<Tuple<string, string, string>>
+        {
+            new Tuple< string, string, string >("4", "3","2"),
+            new Tuple< string, string, string >("6", "6","6"),
+            new Tuple< string, string, string >("7", "7","7"),
+            new Tuple < string, string, string > ("3", "3", "3")
+        };
+        Tuple<string, string, string> winner;
+        bool result = extension.winningThree(hand, out winner);
+
+        Assert.That(result, Is.True);
+
+        Assert.IsTrue(winner.Item1 == "7" && winner.Item2 == "7" && winner.Item3 == "7");
+
+    }
+
+    [TestCase("2", 2)]
+    [TestCase("3", 3)]
+    [TestCase("4", 4)]
+    [TestCase("5", 5)]
+    [TestCase("6", 6)]
+    [TestCase("7", 7)]
+    [TestCase("8", 8)]
+    [TestCase("9", 9)]
+    [TestCase("10", 10)]
+    [TestCase("J", 11)]
+    [TestCase("Q", 12)]
+    [TestCase("K", 13)]
+    [TestCase("A", 14)]
+    public void TestCards(string card, int value)
+    {
+        Extension extension = new Extension();
+        int result = extension.GetValueOfCard(card);
+        Assert.That(result, Is.EqualTo(value));
+    }
+
+}
