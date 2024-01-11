237bc48c02d3b1c696b4f4031d8044770e997793
Stian Gaustad <stian.gaustad@no.experis.com>
2024-01-11 14:53:19 +0100

    Ungraded Extension - Added Player

---
+++ exercise.main/Player.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise.main
+{
+    public class Player
+    {
+        string _playerName;
+        List<Card> _playerCards = new List<Card>();
+
+        public Player(string name) 
+        {
+            _playerName = name;
+        }
+
+        public string getPlayerName() 
+        {
+            return _playerName;
+        }
+
+        public void takeCard(Card card) 
+        {
+            _playerCards.Add(card);
+        }
+
+        public void clearCarcs() 
+        {
+            _playerCards.RemoveAll(x => true);
+        }
+
+        public Card playCard() 
+        {
+            Card res = _playerCards.First();
+            _playerCards.RemoveAt(0);
+            return res;
+        }
+    }
+}
