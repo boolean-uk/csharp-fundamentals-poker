eaa33f5fad5395e8ba86f9609dbb55d422078548
llllllll-l <vincent.werelius@gmail.com>
2024-01-11 11:39:10 +0100

    Extension done and tested

--- exercise.main/Extension.cs
+++ exercise.main/Extension.cs
@@ -8,13 +8,39 @@ namespace exercise.main
 {
     public class Extension
     {
+
+         // Poker cards and there value
+        private Dictionary<string, int> cards = new Dictionary<string, int> {
+            {"2",2}, {"3",3}, {"4",4}, {"5",5}, {"6",6}, {"7",7}, {"8",8},{"9",9},{"10",10},{"J",11},{"Q",12},{"K",13},{"A",14}
+        };
+
         //TODO: complete the following method, keeping the signature the same
         public bool winningThree(IEnumerable<Tuple<string, string, string>> hand, out Tuple<string, string, string> result)
         {
             result = new Tuple<string, string, string>(string.Empty, string.Empty, string.Empty);
 
+            //TODO find a pair
+            foreach (var item in hand) {
+                if (item.Item1.Equals(item.Item2) && item.Item1.Equals(item.Item3)) { //!Pair found
+                    if (checkHighestPair(item.Item1, result.Item1)) {
+                        result = item;
+                    }
+                    
+                }
+            }
+
+            return result.Item1!=string.Empty ? true : false;
+        }
+
+        private bool checkHighestPair(string toup, string res) {
+            cards.TryGetValue(toup, out int toupVal);
+            cards.TryGetValue(res, out int resVal);
+            if (toupVal > resVal) {
+                return true;
+            }
             return false;
         }
 
+        
     }
 }
--- exercise.tests/ExtensionTest.cs
+++ exercise.tests/ExtensionTest.cs
@@ -8,7 +8,7 @@ namespace exercise.tests
 {
     public class ExtensionTest {
 
-    [Test]
+        [Test]
         public void Scenario1() {
        
 
@@ -25,6 +25,52 @@ namespace exercise.tests
             Assert.That(result, Is.False);
             Assert.That(winner.Item1, Is.EqualTo(String.Empty));
             Assert.That(winner.Item2, Is.EqualTo(String.Empty));
+            Assert.That(winner.Item3, Is.EqualTo(String.Empty));
+
+        }
+
+        [Test]
+        public void Scenario1b() {
+       
+
+            Extension core = new Extension();
+       
+            List<Tuple<string, string, string>> hand = new List<Tuple<string, string, string>> { 
+                new Tuple<string, string, string>("K", "K", "K"),
+                new Tuple<string, string, string>("3","7", "5")
+            };
+            Tuple<string, string, string> winner;
+        
+            bool result = core.winningThree(hand, out winner);
+
+            Assert.That(result, Is.True);
+            Assert.That(winner.Item1, Is.EqualTo("K"));
+            Assert.That(winner.Item2, Is.EqualTo("K"));
+            Assert.That(winner.Item2, Is.EqualTo("K"));
+
+        }
+
+        [Test]
+        public void Scenario2() {
+       
+
+            Extension core = new Extension();
+       
+            List<Tuple<string, string, string>> hand = new List<Tuple<string, string, string>> { 
+                new Tuple<string, string, string>("3","3", "3"),
+                new Tuple<string, string, string>("4","4", "4"),
+                new Tuple<string, string, string>("5","5", "5"),
+                new Tuple<string, string, string>("7", "7", "7"),
+                new Tuple<string, string, string>("6","6", "6"),
+                
+            };
+            Tuple<string, string, string> winner;
+        
+            bool result = core.winningThree(hand, out winner);
+
+            Assert.That(result, Is.True);
+        
+            Assert.IsTrue(winner.Item1=="7" && winner.Item2=="7" && winner.Item3 == "7");
 
         }
     }
