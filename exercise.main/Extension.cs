894598e432931d9467842f1dc6544d3fb68573ce
Kristian <kristian.hjelmtveit@no.experis.com>
2024-01-11 14:49:41 +0100

    solution pt1

--- exercise.main/Extension.cs
+++ exercise.main/Extension.cs
@@ -13,7 +13,31 @@ namespace exercise.main
         {
             result = new Tuple<string, string, string>(string.Empty, string.Empty, string.Empty);
 
-            return false;
+            int winningHand = 0;
+
+            foreach (var triplet in hand)
+            {
+                if (triplet.Item1 == triplet.Item2 && triplet.Item1 == triplet.Item3 && GetValueOfCard(triplet.Item1) > winningHand)
+                {
+                    winningHand = GetValueOfCard(triplet.Item1);
+                    result = triplet;
+                }
+            }
+            return winningHand > 0;
+        }
+
+        public int GetValueOfCard(string card)
+        {
+            if (Int32.TryParse(card, out int result)) return result;
+
+            switch (card.ToUpper())
+            {
+                case "J": { return 11; break; }
+                case "Q": { return 12; break; }
+                case "K": { return 13; break; }
+                case "A": { return 14; break; }
+                default: return 0;
+            }
         }
 
     }
