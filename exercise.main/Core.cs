92e9e87a1e5918c0b3a1ab688a780395ecab80fa
Stian Gaustad <stian.gaustad@no.experis.com>
2024-01-11 13:24:54 +0100

    Solved Core tests

--- exercise.main/Core.cs
+++ exercise.main/Core.cs
@@ -10,23 +10,52 @@ namespace exercise.main
 {
     public class Core
     {
-       
-        
+
         //TODO: complete the following method, keeping the signature the same
         public bool winningPair(IEnumerable<Tuple<string, string>> hand, out Tuple<string, string> result)
         {
-            result = new Tuple<string,string>(string.Empty, string.Empty);
-           
+            Tuple<string, string> emptyHand = new Tuple<string, string>(string.Empty, string.Empty);
+            //result.Item1 != string.Empty ? true : false; ;
 
-          
+            IEnumerable<Tuple<string, string>> winningHands = hand
+                .Where(a => (a.Item1 == a.Item2))
+                .OrderByDescending(a => GetValueOfCard(a.Item1));
+            Tuple<string, string> bestHand = winningHands.FirstOrDefault(emptyHand);
 
-          
-
-            return result.Item1!=string.Empty ? true : false;
+            result = bestHand;
+            return (bestHand.Item1 != string.Empty ? true : false);
+            /*
+            result = hand
+                .Where(a => (a.Item1 == a.Item2))
+                .OrderByDescending(a => GetValueOfCard(a.Item1))
+                .FirstOrDefault(emptyHand);
+            */
+            //string var = "hello";
+            return (emptyHand.Item1 == string.Empty ? false: true);
         }
         public int GetValueOfCard(string card)
         {
-            return  0;           
+            int value;
+            switch (card) 
+            {
+                case "J":
+                    value = 11;
+                    break;
+                case "Q":
+                    value = 12;
+                    break;
+                case "K":
+                    value = 13;
+                    break;
+                case "A":
+                    value = 14;
+                    break;
+                default:
+                    int.TryParse(card, out int j);
+                    value = j;
+                    break;
+            }
+            return  value;           
         }
     }
 }
