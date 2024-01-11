8358f6c9cd9a8e1437b49e2e4e3df3c89fea4a04
llllllll-l <vincent.werelius@gmail.com>
2024-01-11 13:58:08 +0100

    functions in Texas Hold'em poker

--- exercise.main/Deck.cs
+++ exercise.main/Deck.cs
@@ -7,6 +7,53 @@ namespace exercise.main
 {
     public class Deck
     {
-        
+        //TODO - Implement a `Deck` class that stores a list / dictionary / array of `Card` objects
+        //TODO - The `Deck` should have a method to `Shuffle()` / regenerate the deck
+        //TODO - The `Deck` should have a method to `Deal()` one card from the deck, removing it from the list of cards in the deck
+
+        //? Dictionary of cards
+        private Dictionary<string, Card> cards;
+
+        private Deck() {
+            cards = GenerateDeck();
+        }
+
+        private Dictionary<string, Card> GenerateDeck() {
+
+            Dictionary<string, Card> deck = new Dictionary<string, Card>();
+
+            // Keep all combinations
+            string[] value = { "1","2","3","4","5","6","7","8","9","10","J","Q","K","A" };
+            string[] suit = {"spades","hearts","diamonds","clubs"};
+
+            // generate cards in deck
+            foreach (string _value in value) {
+                
+                foreach (string _suit in suit) {
+                    string cardName = $"{_value} of {_suit}";
+                    deck.Add(cardName, new Card(_value, _suit));
+                }
+            }
+            return deck;
+        }
+
+        public void Shuffle() {
+            Random rd = new Random();
+            List<KeyValuePair<string, Card>> shuffleDeck = cards.ToList();
+
+            // Random order
+            for (int i = 0; i < shuffleDeck.Count; i++) {
+                int tmp = rd.Next(i-1);
+                var value = shuffleDeck[tmp];
+                shuffleDeck[tmp] = shuffleDeck[i];
+                shuffleDeck[i] = value;
+            }
+
+            cards = new Dictionary<string, Card>(shuffleDeck);
+        }
+
+        public void Deal() {
+
+        }
     }
 }
--- exercise.main/Player.cs
+++ exercise.main/Player.cs
@@ -7,6 +7,30 @@ namespace exercise.main
 {
     public class Player
     {
-        
+        private string name;
+        private string hand;
+        public Player(string name) {
+            this.name = name;
+        }
+        /// <summary>
+        /// Hit = addig a card
+        /// </summary>
+        public void Hit() {
+
+        }
+        /// <summary>
+        /// Remove the hand
+        /// </summary>
+        public void Clear() {
+
+        }
+
+        //TODO! Figure out the correct return type
+        public string GetHand() {
+            
+
+            return "";
+        }
+
     }
 }
