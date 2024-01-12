cc839b8a7fe079225414a8d786192360d2a02f6e
SBjer1 <sebastian.engan@no.experis.com>
2024-01-12 08:10:47 +0100

    done extension

---
+++ exercise.tests/ExtensionTest.cs
@@ -0,0 +1,33 @@
+﻿using exercise.main;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise.tests
+{
+    public class ExtensionTest
+    {
+        [Test]
+        public void Scenario4()
+        {
+            Extension extension = new Extension();
+
+            List<Tuple<string, string, string>> hand = new List<Tuple<string, string, string>>
+            {
+                new Tuple<string, string, string>("4", "3", "8"),
+                new Tuple<string, string, string>("6", "6", "6"),
+                new Tuple<string, string, string>("7", "7", "7"),
+                new Tuple<string, string, string>("3","3", "3")
+            };
+            Tuple<string, string, string> winner;
+            bool result = extension.winningThree(hand, out winner);
+
+            Assert.That(result, Is.True);
+
+            Assert.IsTrue(winner.Item1 == "7" && winner.Item2 == "7" && winner.Item3 == "7");
+
+        }
+    }
+}
