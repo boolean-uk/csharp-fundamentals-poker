70ecba064fc2130b30fc55cfa4f4f3b8cc1fb81d
Caroline Berg <caroline.berg@no.experis.com>
2024-01-12 09:55:17 +0100

    core-poker challenge completed - working on extensions

--- exercise.main/Core.cs
+++ exercise.main/Core.cs
@@ -12,22 +12,84 @@ namespace exercise.main
     {
         //Test before making a pull request
        
-        
+        Dictionary<string, int> cardValues = new Dictionary<string, int>();
+        int temporaryWinner = 0;
+        string tempWinnerstring = "";
+
+
         //TODO: complete the following method, keeping the signature the same
         public bool winningPair(IEnumerable<Tuple<string, string>> hand, out Tuple<string, string> result)
         {
             result = new Tuple<string,string>(string.Empty, string.Empty);
-           
+                    
 
-          
+            foreach ( var pair in hand)
+            {
 
-          
+                if(pair.Item1 == pair.Item2)
+                { 
+                    int valueOfCard = GetValueOfCard(pair.Item1);
+                    
+                    if (valueOfCard > temporaryWinner) { 
+                     
+                    temporaryWinner = cardValues[pair.Item1];
+                    tempWinnerstring = pair.Item1;
+                    result = new Tuple<string, string>(tempWinnerstring, tempWinnerstring);
+                    }            
+                }
+            }
 
             return result.Item1!=string.Empty ? true : false;
         }
-        public int GetValueOfCard(string card)
+
+        public bool winningtriple(IEnumerable<Tuple<string, string, string>> hand, out Tuple<string, string, string> result)
         {
-            return  0;           
+            result = new Tuple<string, string,string>(string.Empty, string.Empty, string.Empty);
+
+            foreach (var pair in hand)
+            {
+
+                if (pair.Item1 == pair.Item2 && pair.Item1 == pair.Item3)
+                {
+                    int valueOfCard = GetValueOfCard(pair.Item1);
+
+                    if (valueOfCard > temporaryWinner)
+                    {
+
+                        temporaryWinner = cardValues[pair.Item1];
+                        tempWinnerstring = pair.Item1;
+                        result = new Tuple<string, string, string>(tempWinnerstring, tempWinnerstring, tempWinnerstring);
+                    }
+                }
+            }
+
+            return result.Item1 != string.Empty ? true : false;
+
+        }
+
+
+            public int GetValueOfCard(string card)
+        {
+
+            if (cardValues.Count ==0 ) { populateDictionary(); }
+
+            cardValues.TryGetValue(card, out int value);
+
+            return  value;           
+        }
+        
+        public void populateDictionary()
+        {
+            for( int i = 2; i<=10;i++ )
+            {
+                cardValues.Add($"{i}", i);
+            }
+           
+            cardValues.Add("A", 14);
+            cardValues.Add("K", 13);
+            cardValues.Add("Q",12);
+            cardValues.Add("J",11);
+
         }
     }
 }
--- exercise.tests/CoreTests.cs
+++ exercise.tests/CoreTests.cs
@@ -11,7 +11,6 @@ public class Tests
     public void Scenario1()
     {
        
-
         Core core = new Core();
        
         List<Tuple<string, string>> hand = new List<Tuple<string, string>> 
@@ -55,8 +54,6 @@ public class Tests
     [Test]
     public void Scenario2()
     {
-
-
         Core core = new Core();
 
         List<Tuple<string, string>> hand = new List<Tuple<string, string>>
@@ -114,4 +111,11 @@ public class Tests
         Assert.That(result, Is.EqualTo(value));
     }
 
+    [TestCase("A", 14)]
+    public void getCardValue(string card, int value)
+    {
+        Core core = new Core();
+        int result = core.GetValueOfCard(card);
+        Assert.That(value, Is.EqualTo(result));
+    }
 }
