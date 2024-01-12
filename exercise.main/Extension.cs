dfc154c13196ce7fc3aed66d513aee41bf942a6d
Aleksander Kolsrud <kols203@gmail.com>
2024-01-12 11:37:23 +0100

    Aleksander Kolsrud C#ex3

--- exercise.main/Extension.cs
+++ exercise.main/Extension.cs
@@ -13,8 +13,39 @@ namespace exercise.main
         {
             result = new Tuple<string, string, string>(string.Empty, string.Empty, string.Empty);
 
-            return false;
+
+            foreach (var cards in hand)
+            {
+                if (cards.Item1 == cards.Item2 && cards.Item2 == cards.Item3)
+                {
+                    if (result.Item1 != string.Empty && GetValueOfCard(cards.Item1) > GetValueOfCard(result.Item1))
+                    {
+                        result = cards;
+                    }
+                    else if (result.Item1 == string.Empty)
+                    {
+                        result = cards;
+                    }
+                }
+            }
+
+
+            return result.Item1 != string.Empty ? true : false;
         }
+        public int GetValueOfCard(string card)
+        {
+            Dictionary<string, int> _cv = new Dictionary<string, int>()
+            {
+                {"1", 1}, {"2", 2}, {"3", 3},{"4", 4}, {"5", 5}, {"6", 6},
+                {"7", 7}, {"8", 8}, {"9", 9}, {"10", 10}, {"J", 11}, {"Q", 12},
+                {"K", 13}, {"A", 14}
+            };
+
+            return _cv[card];
+
+
+        }
+
 
     }
 }
