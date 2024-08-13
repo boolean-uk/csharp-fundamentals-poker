3250562dc9e89640bf241dc73535242de491eb82
Felix <felix.mathiasson99@gmail.com>
2024-08-13 14:12:42 +0200

    core complete

--- exercise.main/Core.cs
+++ exercise.main/Core.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Metrics;
 using System.Linq;
 using System.Reflection;
 using System.Runtime.InteropServices;
@@ -18,15 +19,46 @@ namespace exercise.main
         //TODO: complete the winningPair method, without chaning the method signature
         public bool winningPair(IEnumerable<Tuple<string, string>> hands, out Tuple<string, string> result)
         {
-            result = new Tuple<string,string>(string.Empty, string.Empty);
+            int highestPair = 0;
+            string highestFace = string.Empty;
+            foreach (var pair in hands)
+            {
+                if (pair.Item1 == pair.Item2) //is pair
+                {
+                    if (GetValueOfCard(pair.Item2) > highestPair)
+                    {
+                        highestPair = GetValueOfCard(pair.Item2);
+                        highestFace = pair.Item2;
+                    }
+                }
+            }
 
-            // your code here...          
+            result = new Tuple<string,string>(highestFace, highestFace);
 
             return result.Item1!=string.Empty ? true : false;
         }
         public int GetValueOfCard(string card)
         {
-            return  0;           
+            Dictionary<string, int> cardValues = new Dictionary<string, int>();
+            cardValues.Add("2", 2);
+            cardValues.Add("3", 3);
+            cardValues.Add("4", 4);
+            cardValues.Add("5", 5);
+            cardValues.Add("6", 6);
+            cardValues.Add("7", 7);
+            cardValues.Add("8", 8);
+            cardValues.Add("9", 9);
+            cardValues.Add("10", 10);
+            cardValues.Add("J", 11);
+            cardValues.Add("Q", 12);
+            cardValues.Add("K", 13);
+            cardValues.Add("A", 14);
+
+            if(cardValues.ContainsKey(card))
+            {
+                return cardValues[card];
+            }
+            return  0;  //failed case         
         }
     }
 }
