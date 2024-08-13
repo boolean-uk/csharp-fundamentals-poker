c8469ea73cb830f64dc66b5b256bf6557be049fb
Joaquin Lindkvist <joaquinzache@gmail.com>
2024-08-13 16:12:13 +0200

    Completed the tasks and extension tasks? (No tests given)
    Also implemented the texas hold'em part of the assignment but without calculating the winner.

--- exercise.main/Core.cs
+++ exercise.main/Core.cs
@@ -21,12 +21,47 @@ namespace exercise.main
             result = new Tuple<string,string>(string.Empty, string.Empty);
 
             // your code here...          
+            int winningScore = 0;
+            foreach (var pair in hands)
+            {
+                if(pair.Item1 == pair.Item2)
+                {
+                    int score = GetValueOfCard(pair.Item1) + GetValueOfCard(pair.Item2);
+                    if (winningScore < score)
+                    {
+                        winningScore = score;
+                        result = pair;
+                    }
+                }
+                
+            }
 
             return result.Item1!=string.Empty ? true : false;
         }
         public int GetValueOfCard(string card)
         {
-            return  0;           
+            Dictionary<string, int> cardValue = new Dictionary<string, int>()
+            {
+                { "2", 2 },
+                { "3", 3 }, 
+                { "4", 4 },
+                { "5", 5 },
+                { "6", 6 },
+                { "7", 7 },
+                { "8", 8 },
+                { "9", 9 },
+                { "10", 10 },
+                { "J", 11 },
+                { "Q", 12 },
+                { "K", 13 },
+                { "A", 14 }
+            };
+
+            if(cardValue.ContainsKey(card))
+            {
+                return cardValue[card];
+            }
+            return  0; //No such card exists.           
         }
     }
 }
---
+++ exercise.main/Player.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise.main
+{
+    public class Player
+    {
+        public string name { get; set; } = string.Empty;
+        public List<Card> hand = new List<Card>();
+
+        public Player(string name) 
+        {
+            this.name = name;
+        }
+
+        public void AddCard(Card card)
+        { 
+            hand.Add(card); 
+        }
+
+        public void ClearHand()
+        {
+            hand.Clear(); 
+        }
+    }
+}
