dd12c8ec6492980b74f9044a65e4171c59d397ec
Andreas Conradi Nitschke <andreas.nitschke@experisacademy.no>
2025-01-09 14:05:31 +0100

    finished core+extension

--- exercise.main/Extension.cs
+++ exercise.main/Extension.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,12 +10,27 @@ namespace exercise.main
     public class Extension
     {
         //TODO: complete the following method, keeping the signature the same
-        public bool winningThree(IEnumerable<Tuple<string, string, string>> hand, out Tuple<string, string, string> result)
+        public bool winningThree(IEnumerable<Tuple<string, string, string>> hands, out Tuple<string, string, string> result)
         {
             result = new Tuple<string, string, string>(string.Empty, string.Empty, string.Empty);
 
-            return false;
+
+            CardHandler handler = new CardHandler();
+            var winningValue = 0;
+            var currentIndex = 0;
+            foreach (var item in hands)
+            {
+                if (handler.isTriplet(item) && handler.GetValueOfCard(item.Item1) > winningValue)
+                {
+                    result = item;
+                    winningValue = handler.GetValueOfCard(item.Item2);
+                }
+                currentIndex++;
+            }
+            return result.Item1 != string.Empty ? true : false;
         }
 
+
     }
+
 }
